42387e5a797c41ec013304a1fd0ad5bca1069dd4
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2023-01-12 11:46:50 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1 - Sumatoria");
+
+            Console.WriteLine("Ingrese Primer numero");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese Segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese Tercero numero");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado de la suma es " + functionSuma(num1, num2, num3));
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Ejercicio 2 - Clase Coche");
+
+            Coche cochecito = new Coche();
+            cochecito.IncrementoPuertas();
+            cochecito.IncrementoPuertas();
+            cochecito.IncrementoPuertas();
+            cochecito.IncrementoPuertas();
+
+            Console.WriteLine("El número de puertas que tiene  el carro es: " + cochecito.TotalPuertas());
+
+
+            Console.ReadKey();
+        }
+
+        static int functionSuma(int a, int b, int c)
+        {
+            int resultado = a + b + c;
+            return resultado;
+        }
+
+        public class Coche
+        {
+            int puertas = 0;
+
+            public int IncrementoPuertas()
+            {
+                puertas++;
+                return puertas=+puertas;
+            }
+
+            public int TotalPuertas()
+            {
+                return puertas;
+            }
+        }
+    }
+}
